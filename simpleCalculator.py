9854b3011114cc5408eaf81da0f21953b12e9f4e
sp2728 <sp2728@njit.edu>
2020-03-18 14:16:22 -0400

    pushing the simple calculator code with CSV files

---
+++ simpleCalculator.py
@@ -0,0 +1,89 @@
+
+class calculator:
+
+    #Addition
+    @staticmethod
+    def add(num1, num2):
+        return round(num1 + num2, 6)
+
+    #Subtraction
+    @staticmethod
+    def subtraction(num1, num2):
+        return num1 - num2
+
+    #Multiplication
+    @staticmethod
+    def multiply(num1, num2):
+        return num1 * num2
+
+    #Division
+    @staticmethod
+    def divide(num1, num2):
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            print("Divide by Zero is not possible")
+
+    #Sqaure of a number
+    @staticmethod
+    def square(num1):
+        return num1 * num1
+
+    #Squareroot of a number
+    @staticmethod
+    def squareRoot(num1):
+        try:
+            return round((num1)**(1/2),8)
+        except ValueError:
+            print("Invalid Input")
+
+
+
+if __name__ == '__main__':
+
+    choice = ""
+
+    
+
+    while choice != "7":
+        print("Select the operation:")
+        print("1. Add")
+        print("2. Subtraction")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Square")
+        print("6. Square Root")
+        print("7. Quit")
+
+        choice = input("Enter Choice: ")
+
+        if choice == "1":
+            firstNum = float(input("Enter first number"))
+            secondNum = float(input("Enter Second Number"))
+            print(calculator.add(firstNum, secondNum))
+
+        elif choice == "2":
+            firstNum = float(input("Enter first number"))
+            secondNum = float(input("Enter Second Number"))
+            print(calculator.subtraction(firstNum, secondNum))
+
+        elif choice == "3":
+            firstNum = float(input("Enter first number"))
+            secondNum = float(input("Enter Second Number"))
+            print(calculator.multiply(firstNum, secondNum))
+
+        elif choice == "4":
+            firstNum = float(input("Enter first number"))
+            secondNum = float(input("Enter Second Number"))
+            print(calculator.divide(firstNum, secondNum))
+        elif choice == "5":
+            firstNum = float(input("Enter first number"))
+            print(calculator.square(firstNum))
+        elif choice == "6":
+            firstNum = float(input("Enter first number"))
+            print(calculator.squareRoot(firstNum))
+        elif choice == "7":
+            break
+
+        else:
+            print("Invalid Choice")
---
+++ tests/test_calc.py
@@ -0,0 +1,104 @@
+
+from simpleCalculator import calculator
+import pytest
+import unittest
+import csv
+
+
+
+class test_PythonSimpleCalc(unittest.TestCase):
+    def test_calc(self):
+        assert calculator
+
+    def test_calc_add(self):
+        num1=[]
+        num2 = []
+        res = []
+        i=0
+        with open('Unit Test Addition.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                num2.append(row[1])
+                res.append(row[2])
+            while(i!=len(num1)):
+               assert calculator.add(int(num1[i]), int(num2[i])) == int(res[i])
+               i=i+1;
+
+
+    def test_calc_subtract(self):
+        num1=[]
+        num2 = []
+        res = []
+        i=0
+        with open('Unit Test Subtraction.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                num2.append(row[1])
+                res.append(row[2])
+            while(i!=len(num1)):
+               assert calculator.subtraction(int(num2[i]), int(num1[i])) == int(res[i])
+               i=i+1;
+
+    def test_calc_multiply(self):
+        num1 = []
+        num2 = []
+        res = []
+        i = 0
+        with open('Unit Test Multiplication.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                num2.append(row[1])
+                res.append(row[2])
+            while (i != len(num1)):
+                assert calculator.multiply(int(num2[i]), int(num1[i])) == int(res[i])
+                i = i + 1;
+
+    def test_calc_divide(self):
+        num1 = []
+        num2 = []
+        res = []
+        i = 0
+        with open('Unit Test Division.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                num2.append(row[1])
+                res.append(row[2])
+            while (i != len(num1)):
+                assert round(calculator.divide(float(num2[i]), float(num1[i])), 9) == float(res[i])
+                i = i + 1;
+
+    def test_calc_square(self):
+        num1 = []
+        res = []
+        i = 0
+        with open('Unit Test Square.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                res.append(row[1])
+            while (i != len(num1)):
+                assert calculator.square(int(num1[i])) == int(res[i])
+                i = i + 1;
+
+    def test_calc_squareroot(self):
+        num1 = []
+        res = []
+        i = 0
+        with open('Unit Test Square Root.csv') as csvfile:
+            readCSV = csv.reader(csvfile)
+            next(readCSV)
+            for row in readCSV:
+                num1.append(row[0])
+                res.append(row[1])
+            while (i != len(num1)):
+                assert round(calculator.squareRoot(float(num1[i])),8) == round(float(res[i]),8)
+                i = i + 1;
