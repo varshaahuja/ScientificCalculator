6070a41404b666a9d75154ecb4b69b0bb47652a8
Jinalshah12345 <js2565@njit.edu>
2020-03-24 03:12:40 +0530

    Adding Test Files

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,16 +1,17 @@
-
 from simpleCalculator import simpleCalculator as calculator
 import pytest
 import unittest
 import csv
+from scientificCalculator import scientificCalculator as scientificCalculator
 
 
 
 class test_PythonSimpleCalc(unittest.TestCase):
-    def test_calc(self):
+
+def test_calc(self):
         assert calculator
 
-    def test_calc_add(self):
+def test_calc_add(self):
         num1=[]
         num2 = []
         res = []
@@ -27,7 +28,7 @@ class test_PythonSimpleCalc(unittest.TestCase):
                i=i+1;
 
 
-    def test_calc_subtract(self):
+def test_calc_subtract(self):
         num1=[]
         num2 = []
         res = []
@@ -43,7 +44,7 @@ class test_PythonSimpleCalc(unittest.TestCase):
                assert calculator.subtraction(int(num2[i]), int(num1[i])) == int(res[i])
                i=i+1;
 
-    def test_calc_multiply(self):
+def test_calc_multiply(self):
         num1 = []
         num2 = []
         res = []
@@ -59,7 +60,7 @@ class test_PythonSimpleCalc(unittest.TestCase):
                 assert calculator.multiply(int(num2[i]), int(num1[i])) == int(res[i])
                 i = i + 1;
 
-    def test_calc_divide(self):
+def test_calc_divide(self):
         num1 = []
         num2 = []
         res = []
@@ -75,7 +76,7 @@ class test_PythonSimpleCalc(unittest.TestCase):
                 assert round(calculator.divide(float(num2[i]), float(num1[i])), 9) == float(res[i])
                 i = i + 1;
 
-    def test_calc_square(self):
+def test_calc_square(self):
         num1 = []
         res = []
         i = 0
@@ -89,7 +90,7 @@ class test_PythonSimpleCalc(unittest.TestCase):
                 assert calculator.square(int(num1[i])) == int(res[i])
                 i = i + 1;
 
-    def test_calc_squareroot(self):
+def test_calc_squareroot(self):
         num1 = []
         res = []
         i = 0
@@ -102,3 +103,49 @@ class test_PythonSimpleCalc(unittest.TestCase):
             while (i != len(num1)):
                 assert round(calculator.squareRoot(float(num1[i])),8) == round(float(res[i]),8)
                 i = i + 1;
+
+
+def test_calc_calculate_median(self):
+
+        input = csv.reader(open("median.csv"))
+        next(input)
+        list_of_floats = []
+        for row in input:
+            for item in row[0:10]:
+                list_of_floats.append(float(item))
+            assert  float(scientificCalculator.calculate_median((list_of_floats))) == float(row[10])
+
+def test_calc_stdev(self):
+
+        input = csv.reader(open("sampleStandardDeviation.csv"))
+        next(input)
+        list_of_floats = []
+        for row in input:
+            for item in row[0:10]:
+                list_of_floats.append(float(item))
+            assert float(scientificCalculator.sampleStandardDeviation((list_of_floats))) == float(row[10])
+
+
+def test_calc_population_proportion_variance(self):
+
+        input = csv.reader(open("varianceOfPopulationProportion.csv"))
+        next(input)
+        list_of_floats = []
+        for row in input:
+            for item in row[0:10]:
+                list_of_floats.append(float(item))
+            assert  float(scientificCalculator.population_proportion_variance((list_of_floats), int(row[11]))) == float(row[10])
+
+
+def test_calc_correlationCoefficient(self):
+
+        input = csv.reader(open("populationCoefficientCorrelation.csv"))
+        next(input)
+        list_of_floats = []
+        for row in input:
+            for item in row[0:10]:
+                list_of_floats.append(float(item))
+            assert float(scientificCalculator.correlationCoefficient((list_of_floats))) == float(row[10])
+
+
+
