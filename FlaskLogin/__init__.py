b755ff98a9db5fad15a6356196e2c69ce42feda0
sp2728 <sp2728@njit.edu>
2020-04-02 23:56:12 -0400

    added the bootstrap plugin

--- FlaskLogin/__init__.py
+++ FlaskLogin/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -13,7 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
+    Bootstrap(app)
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
