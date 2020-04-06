a05e4670f0919a503cd207bf2d97eaaebd9df07f
sp2728 <sp2728@njit.edu>
2020-04-06 00:22:11 -0400

    updated the clear history code

--- FlaskLogin/main.py
+++ FlaskLogin/main.py
@@ -4,7 +4,6 @@ from simpleCalculator import simpleCalculator as calculatorfun
 from . import db
 from .models import History, User
 
-
 main = Blueprint('main', __name__)
 
 
@@ -18,49 +17,55 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
+
 @main.route('/calculator')
 @login_required
 def calculator():
     all_data = History.query.filter(History.email == current_user.email)
-    return render_template('calculator.html', name=current_user.name, ALLhistory = all_data)
+    return render_template('calculator.html', name=current_user.name, ALLhistory=all_data)
+
 
 @main.route('/calculator', methods=['POST'])
 def calc_post():
     calc = request.form.get('res')
     first = calc.split('z')
 
-    if(first[1]== '+'):
+    if (first[1] == '+'):
         res = calculatorfun.add(float(first[0]), float(first[2]))
-        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)), email=current_user.email)
+        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)),
+                          email=current_user.email)
 
 
     elif (first[1] == '-'):
         res = calculatorfun.subtraction(float(first[0]), float(first[2]))
-        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)), email=current_user.email)
+        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)),
+                          email=current_user.email)
 
 
     elif (first[1] == '/'):
         res = calculatorfun.divide(float(first[0]), float(first[2]))
-        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)), email=current_user.email)
+        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)),
+                          email=current_user.email)
 
 
     elif (first[1] == '*'):
         res = calculatorfun.multiply(float(first[0]), float(first[2]))
-        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)), email=current_user.email)
+        history = History(num1=float(first[0]), num2=float(first[2]), op=first[1], res=float(round(res, 5)),
+                          email=current_user.email)
 
 
     elif (first[1] == '%'):
-        res = float(float(first[0])/100)
+        res = float(float(first[0]) / 100)
         history = History(num1=float(first[0]), op=first[1], res=float(round(res, 5)), email=current_user.email)
 
 
     elif (first[1] == 'invSq'):
-        res = float(1/calculatorfun.square(float(first[0])))
+        res = float(1 / calculatorfun.square(float(first[0])))
         history = History(num1=float(first[0]), op=first[1], res=float(round(res, 5)), email=current_user.email)
 
 
     elif (first[1] == 'invSqrt'):
-        res = float(1/calculatorfun.squareRoot(float(first[0])))
+        res = float(1 / calculatorfun.squareRoot(float(first[0])))
         history = History(num1=float(first[0]), op=first[1], res=float(round(res, 5)), email=current_user.email)
 
 
@@ -76,9 +81,10 @@ def calc_post():
     db.session.add(history)
     db.session.commit()
 
-    all_data = History.query.filter(History.email==current_user.email)
+    all_data = History.query.filter(History.email == current_user.email)
 
     return render_template('calculator.html', result=res, ALLhistory=all_data)
+
 
 @main.route('/delete/<int:id>')
 def delete(id):
@@ -90,12 +96,11 @@ def delete(id):
 
     return render_template('calculator.html', ALLhistory=all_data)
 
+
 @main.route('/clear')
 def clearHistory():
-    all_data = History.query.filter(History.email == current_user.email)
-
-    db.session.delete(all_data)
+    all_data = History.query.filter(History.email == current_user.email).delete()
     db.session.commit()
 
-    all_data = History.query.filter(History.email == current_user.email)
-    return render_template('calculator.html', ALLhistory=all_data)
+    all_data1 = History.query.filter(History.email == current_user.email)
+    return render_template('calculator.html', ALLhistory=all_data1)
